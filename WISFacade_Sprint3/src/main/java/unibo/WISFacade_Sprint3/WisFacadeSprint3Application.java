4420d44030063057440c3d6810cc68c0c8992e6f
jjulespop <giuly.fatti@gmail.com>
2024-11-18 19:30:35 +0100

    added first version of wis spring facade, TODO: integrate frontend

---
+++ WISFacade_Sprint3/src/main/java/unibo/WISFacade_Sprint3/WisFacadeSprint3Application.java
@@ -0,0 +1,13 @@
+package main.java.unibo.WISFacade_Sprint3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisFacadeSprint3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisFacadeSprint3Application.class, args);
+	}
+
+}
